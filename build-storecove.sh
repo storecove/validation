bda5be32cf0236bc583bf828becc50582858abeb
Michael Riviera <mriviera@ri4a.com>
2018-05-05 22:28:49 +0200

    add storecove build script

---
+++ build-storecove.sh
@@ -0,0 +1,7 @@
+echo
+echo
+echo "To create SI-UBL-INV-1.1.xsl edit SI-UBL-INV.SCH and comment out the 1.2 stuff"
+echo "vim SI-UBL-INV.SCH"
+echo
+echo
+./convert_linux.sh SI-UBL-INV.SCH SI-UBL-INV-1.1.xsl
